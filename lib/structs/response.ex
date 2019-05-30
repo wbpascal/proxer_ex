b3297d4a92ed706ac5ff73b51d534a8bbc3cdd10
Pascal Wiedenbeck <infinitesoul155@gmail.com>
2019-05-30 11:50:49 +0200

    Remove old Poison.Decoder implementation

--- lib/structs/response.ex
+++ lib/structs/response.ex
@@ -9,13 +9,3 @@ defmodule ProxerEx.Response do
         }
   defstruct [:error, :message, :data, :code]
 end
-
-defimpl Poison.Decoder, for: ProxerEx.Response do
-  def decode(%ProxerEx.Response{error: error} = value, _options) do
-    %{value | :error => error == 1}
-  end
-
-  def decode(value, _options) do
-    value
-  end
-end
